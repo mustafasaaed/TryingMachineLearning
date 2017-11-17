78f89cd762dc177d312bf0719edc96f777a342d9
mustafa <devmustafasaeed@outlook.com>
2017-11-17 21:49:41 +0200

    finishing operations

--- OpenCv/ImagesOperations/Operations2.py
+++ OpenCv/ImagesOperations/Operations2.py
@@ -1,11 +1,14 @@
 import numpy as np
 import cv2
 
-img1 = cv2.imread('..\Images\3D-Matplotlib.png')
-img2 = cv2.imread('..\Images\mainsvmimage.png')
+img1 = cv2.imread('../Images/3D-Matplotlib.png')
+img2 = cv2.imread('../Images/mainsvmimage.png')
 
-add = img1 + img2
+# add = img1 + img2
+#add = cv2.add(img1,img2)
 
-cv2.imshow('add', add)
+weighted = cv2.addWeighted(img1, 0.6, img2, .4, 0) 
+
+cv2.imshow('weighted', weighted)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
