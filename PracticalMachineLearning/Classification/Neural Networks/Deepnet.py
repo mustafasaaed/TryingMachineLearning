1c0551861475160ed057909f56233c1714f69b17
Mustafa Saeed <devmustafasaeed@outlook.com>
2017-11-22 22:43:39 +0200

    Creating NN Model

--- PracticalMachineLearning/Classification/Neural Networks/Deepnet.py
+++ PracticalMachineLearning/Classification/Neural Networks/Deepnet.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-# mnist = input_data.read_data_sets('data', one_hot=True)
+mnist = input_data.read_data_sets('data', one_hot=True)
 
 n_nodes_hl1 = 500
 n_nodes_hl2 = 500
