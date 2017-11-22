13ede3d3ad3dc7b39545d1676872757bc6a8ecf3
Mustafa Saeed <devmustafasaeed@outlook.com>
2017-11-22 22:42:18 +0200

    Creating NN Model

--- PracticalMachineLearning/Classification/Neural Networks/Deepnet.py
+++ PracticalMachineLearning/Classification/Neural Networks/Deepnet.py
@@ -1,15 +1,41 @@
 import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
 
-x1 = tf.constant(5)
-x2 = tf.constant(6)
+# mnist = input_data.read_data_sets('data', one_hot=True)
 
-result = tf.multiply(x1, x2)
-print(result)
+n_nodes_hl1 = 500
+n_nodes_hl2 = 500
+n_nodes_hl3 = 500
 
-# sess = tf.Session()
-# print(sess.run(result))
-# sess.close()
+n_classes = 10
+batch_size = 100
 
-with tf.Session() as sess: 
-    print(sess.run(result))
+x = tf.placeholder('float', [None, 784])
+y = tf.placeholder('float')
+
+
+def neural_network_model(data):
+    hidden_1_layer = {'weights': tf.Variable(tf.random_normal(
+        [784, n_nodes_hl1])), 'biases': tf.Variable(tf.random_normal(n_nodes_hl1))}
+
+    hidden_2_layer = {'weights': tf.Variable(tf.random_normal(
+        [n_nodes_hl1, n_nodes_hl2])), 'biases': tf.Variable(tf.random_normal(n_nodes_hl2))}
+
+    hidden_3_layer = {'weights': tf.Variable(tf.random_normal(
+        [n_nodes_hl2, n_nodes_hl3])), 'biases': tf.Variable(tf.random_normal(n_nodes_hl3))}
+
+    output_layer = {'weights': tf.Variable(tf.random_normal(
+        [n_nodes_hl3, n_classes])), 'biases': tf.Variable(tf.random_normal(n_classes))}
+
+    l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']) + hidden_1_layer['biases'])
+    l1 = tf.nn.relu(l1)
     
+    l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']) + hidden_2_layer['biases'])
+    l2 = tf.nn.relu(l2)
+
+    l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']) + hidden_3_layer['biases'])
+    l3 = tf.nn.relu(l3)
+
+    output = tf.matmul(l2, output_layer['weights']) + output_layer['biases']
+
+    return output
