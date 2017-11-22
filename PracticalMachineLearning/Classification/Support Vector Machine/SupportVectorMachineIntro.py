eadc8548759161dc3c259dc3c615520171414efb
Mustafa Saeed <Mustafa Saeed>
2017-11-22 20:04:56 +0200

    saving SVM For Now

--- PracticalMachineLearning/Classification/Support Vector Machine/SupportVectorMachineIntro.py
+++ PracticalMachineLearning/Classification/Support Vector Machine/SupportVectorMachineIntro.py
@@ -11,7 +11,7 @@ y = np.array(df['class'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
-clf = neighbors.KNeighborsClassifier()
+clf = svm.SVC()
 clf.fit(X_train, y_train)
 
 accuracy = clf.score(X_test, y_test)
